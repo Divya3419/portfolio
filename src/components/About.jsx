0e984dd02fa4d469d9944cc2ab7e3656778f5217
Divya <99958902+Divya3419@users.noreply.github.com>
2023-04-03 22:33:56 +0530

    update Portfolio

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,25 +1,28 @@
-import React from 'react'
+import React from "react";
 import { Fade } from "react-awesome-reveal";
-import './styles.css';
-import resume from '../media/Divya Resume.pdf'
+import "./styles.css";
+import resume from "../media/Divya Resume.pdf";
 
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import { faGithub, faInstagramSquare, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
-import { faFile } from '@fortawesome/free-solid-svg-icons';
-import Card from 'react-bootstrap/Card'
-import Button from 'react-bootstrap/Button'
-import Col from 'react-bootstrap/Col'
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faGithub,
+  faInstagramSquare,
+  faLinkedinIn,
+} from "@fortawesome/free-brands-svg-icons";
+import { faFile } from "@fortawesome/free-solid-svg-icons";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
 
 const About = ({ darkMode }) => {
-
   var TxtRotate = function (el, toRotate, period) {
     this.toRotate = toRotate;
     this.el = el;
     this.loopNum = 0;
     this.period = parseInt(period, 10) || 2000;
-    this.txt = '';
+    this.txt = "";
     this.tick();
     this.isDeleting = false;
   };
@@ -34,17 +37,19 @@ const About = ({ darkMode }) => {
       this.txt = fullTxt.substring(0, this.txt.length + 1);
     }
 
-    this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
+    this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
 
     var that = this;
     var delta = 300 - Math.random() * 100;
 
-    if (this.isDeleting) { delta /= 2; }
+    if (this.isDeleting) {
+      delta /= 2;
+    }
 
     if (!this.isDeleting && this.txt === fullTxt) {
       delta = this.period;
       this.isDeleting = true;
-    } else if (this.isDeleting && this.txt === '') {
+    } else if (this.isDeleting && this.txt === "") {
       this.isDeleting = false;
       this.loopNum++;
       delta = 500;
@@ -56,10 +61,10 @@ const About = ({ darkMode }) => {
   };
 
   window.onload = function () {
-    var elements = document.getElementsByClassName('txt-rotate');
+    var elements = document.getElementsByClassName("txt-rotate");
     for (var i = 0; i < elements.length; i++) {
-      var toRotate = elements[i].getAttribute('data-rotate');
-      var period = elements[i].getAttribute('data-period');
+      var toRotate = elements[i].getAttribute("data-rotate");
+      var period = elements[i].getAttribute("data-period");
       if (toRotate) {
         new TxtRotate(elements[i], JSON.parse(toRotate), period);
       }
@@ -76,82 +81,124 @@ const About = ({ darkMode }) => {
       <br></br>
       <br></br>
       <Fade>
-        <Container align='center'>
+        <Container align="center">
           <Row>
-            <Col lg={8} align='center'>
+            <Col lg={8} align="center">
               <Card className="nbcard">
                 <Card.Body>
-                  <Card.Title><h2 className="lead">Let's have an introduction!</h2></Card.Title>
+                  <Card.Title>
+                    <h2 className="lead">Let's have an introduction!</h2>
+                  </Card.Title>
                   <br></br>
                   <Card.Text>
-                    <p style={{fontSize: "17px",fontWeight: "500"}}>Enthusiastic aspiring full-stack web developer specialized in <strong>  MERN </strong>  stack  having fascination towards computer and technology. Keen to create new and innovative websites by showcasing my skills. Apart from that a great team player, ability to work with different kinds of people with different thinkings.In spare time I like Sketching,Photography and travelling. Gaining skills by which I can create something productive. Currently looking for an job opportunity to work in an interesting company by applying my skills.</p>
+                    <p style={{ fontSize: "17px", fontWeight: "500" }}>
+                      Experienced frontend engineer skilled in HTML, CSS,
+                      JavaScript, and React. Proven track record in
+                      collaborating with cross-functional teams, testing,
+                      debugging, and optimizing website performance. Stay
+                      up-to-date with the latest industry trends and best
+                      practices. Strong communication and problem-solving
+                      skills, consistently delivering high-quality products on
+                      time and on budget.
+                    </p>
                   </Card.Text>
-                 
 
                   <a href={resume} target="_blank" rel="noreferrer noopener">
-                    <Button variant={darkMode ? "outline-light" : "outline-dark"} className='a'>
-                      <FontAwesomeIcon className="dark" size="lg" icon={faFile} /> View Resume
+                    <Button
+                      variant={darkMode ? "outline-light" : "outline-dark"}
+                      className="a"
+                    >
+                      <FontAwesomeIcon
+                        className="dark"
+                        size="lg"
+                        icon={faFile}
+                      />{" "}
+                      View Resume
                     </Button>
                   </a>
 
                   <a href={resume} download>
-                    <Button variant={darkMode ? "outline-light" : "outline-dark"} className='a'>
-                      <FontAwesomeIcon className="dark" size="lg" icon={faFile} /> Download Resume
+                    <Button
+                      variant={darkMode ? "outline-light" : "outline-dark"}
+                      className="a"
+                    >
+                      <FontAwesomeIcon
+                        className="dark"
+                        size="lg"
+                        icon={faFile}
+                      />{" "}
+                      Download Resume
                     </Button>
                   </a>
-                
                 </Card.Body>
               </Card>
             </Col>
-            <Col lg={4} align='center'>
+            <Col lg={4} align="center">
               <Card className="nbcard">
                 <Card.Body>
-                  <Card.Title><h2 className="lead">Let's Connect!</h2></Card.Title>
+                  <Card.Title>
+                    <h2 className="lead">Let's Connect!</h2>
+                  </Card.Title>
                   <br></br>
 
                   <div>
-                    <p>Divya<br /> Jagriti,Vihar,Meerut<br/> <strong style={{fontWeight:"600"}}>8439637030</strong>
+                    <p>
+                      Divya
+                      <br /> Jagriti,Vihar,Meerut
+                      <br />{" "}
+                      <strong style={{ fontWeight: "600" }}>8439637030</strong>
                       <br />
-                      <strong style={{fontWeight:"600"}}>divyajain3419@gmail.com</strong>
+                      <strong style={{ fontWeight: "600" }}>
+                        divyajain3419@gmail.com
+                      </strong>
                     </p>
                   </div>
 
                   <br />
 
-
-
-                 
-
-                  <a className="social-icons" href="https://linkedin.com/in/divya-jain-456a50197" target="_blank" rel="noreferrer noopener">
-                    <Button variant={darkMode ? "outline-light" : "outline-dark"}>
-                      <FontAwesomeIcon className="dark" size="lg" icon={faLinkedinIn} />
+                  <a
+                    className="social-icons"
+                    href="https://linkedin.com/in/divya-jain-456a50197"
+                    target="_blank"
+                    rel="noreferrer noopener"
+                  >
+                    <Button
+                      variant={darkMode ? "outline-light" : "outline-dark"}
+                    >
+                      <FontAwesomeIcon
+                        className="dark"
+                        size="lg"
+                        icon={faLinkedinIn}
+                      />
                     </Button>
                   </a>
 
-
-
-
-
-                  <a className="social-icons" href="https://github.com/Divya3419" target="_blank" rel="noreferrer noopener">
-                    <Button variant={darkMode ? "outline-light" : "outline-dark"}>
-                      <FontAwesomeIcon className="dark" size="lg" icon={faGithub} />
+                  <a
+                    className="social-icons"
+                    href="https://github.com/Divya3419"
+                    target="_blank"
+                    rel="noreferrer noopener"
+                  >
+                    <Button
+                      variant={darkMode ? "outline-light" : "outline-dark"}
+                    >
+                      <FontAwesomeIcon
+                        className="dark"
+                        size="lg"
+                        icon={faGithub}
+                      />
                     </Button>
                   </a>
-
-                 
-                 
-                  
                 </Card.Body>
               </Card>
             </Col>
           </Row>
           <br></br>
           <hr></hr>
-        
         </Container>
       </Fade>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
